c9c52c57eb57b96e551a3d7123acab8944680226
sameom1048 <samvision99@gmail.com>
2023-03-20 21:37:03 +0900

    modify crawling 기초

--- src/main/java/com/hadoop/demo/DemoApplication.java
+++ src/main/java/com/hadoop/demo/DemoApplication.java
@@ -7,7 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DemoApplication.class, args);
+		
+//		CrawlingExample crawlingExample = new CrawlingExample("https://store.steampowered.com/app/1174180/Red_Dead_Redemption_2/");
+//		crawlingExample.process();
+
+		CrawlingPassmark crawlingPassmark = new CrawlingPassmark("https://www.cpubenchmark.net/cpu_list.php");
+		crawlingPassmark.process();
 	}
 
 }
