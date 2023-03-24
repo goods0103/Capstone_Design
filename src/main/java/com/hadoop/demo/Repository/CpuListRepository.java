aa3e6780fd4502dc896f74fcda84dfd2b91a9aa0
sameom1048 <samvision99@gmail.com>
2023-03-24 15:53:25 +0900

    modify

--- src/main/java/com/hadoop/demo/Repository/CpuListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hadoop.demo.Repository;
-
-import com.hadoop.demo.Model.CpuList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CpuListRepository extends JpaRepository<CpuList, String> {
-
-}
