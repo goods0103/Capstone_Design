681d6432c8dc02cc9516081d6373a54ac3b23438
seaking <seaking110@naver.com>
2023-03-27 22:05:41 +0900

    [Mod] 현수꺼 다운 받기위한 커밋

--- src/main/frontend/src/components/eventBanner/InsertSpec.js
+++ src/main/frontend/src/components/eventBanner/InsertSpec.js
@@ -1,7 +1,42 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function insertSpec() {
+function InsertSpec() {
+    const [formState, setFormState] = useState({ cpu: '', gpu: '' });
+    // const [formState, setFormState] = useState({ cpu: '', gpu: '', ram: '' });
+    const handleChange = (e) => {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('/api/submit', formState)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="cpu">Cpu:</label>
+                <input type="text" id="cpu" name="cpu" value={formState.cpu} onChange={handleChange} />
+            </div>
+            <div>
+                <label htmlFor="gpu">Gpu:</label>
+                <input type="text" id="gpu" name="gpu" value={formState.gpu} onChange={handleChange} />
+            </div>
+            {/*<div>*/}
+            {/*    <label htmlFor="ram">Ram:</label>*/}
+            {/*    <input type="text" id="ram" name="ram" value={formState.ram} onChange={handleChange} />*/}
+            {/*</div>*/}
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
-export default insertSpec;
+
+export default InsertSpec;
--- src/main/frontend/src/components/eventBanner/ShowMySpec.js
+++ src/main/frontend/src/components/eventBanner/ShowMySpec.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 function ShowMySpec() {
     const [userSpec, setUserSpec] = useState([]);
-    let waring = "시스템 정보를 불러오기 위한 파일을 다운로드 중입니다. 다운이 완료되면 실행 시켜 주세요"
+    const [waring, setWaring] = useState(["시스템 정보를 불러오기 위한 파일을 다운로드 중입니다. 다운이 완료되면 실행 시켜 주세요"])
 
     useEffect(() => {
         const userData = async () => {
             try {
                 const response = await axios.get('/MySpec');
                 setUserSpec(response.data);
-                waring = "시스템 정보 불러오기 완료";
+                setWaring("시스템 정보 불러오기 완료"); 
             } catch (error) {
                 console.log(error);
             }
@@ -20,6 +20,7 @@ function ShowMySpec() {
     },[waring])
     return (
         <div>
+            {/* <p>{userSpec}</p> */}
             <p>{waring}</p>
         </div>
     );
