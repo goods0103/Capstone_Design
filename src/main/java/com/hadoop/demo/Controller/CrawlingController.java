e07f581a4fe5dee3aeaaa500a3f7e77a07ce2ec9
goods0103 <goods0103@naver.com>
2023-04-02 23:31:31 +0900

    ram crawling 수정

--- src/main/java/com/hadoop/demo/Controller/CrawlingController.java
+++ src/main/java/com/hadoop/demo/Controller/CrawlingController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -104,26 +105,78 @@ public class CrawlingController {
                 double ramWrite = Double.parseDouble(row.select("td:nth-child(4)").text());
 
                 int ramSize = 0;
-                Pattern pattern = Pattern.compile("(\\d+)\\s*(GB|MB|TB|KB)");
+                Pattern pattern = Pattern.compile("(\\d+)\\s*(GB)");
                 Matcher matcher = pattern.matcher(ramName);
                 if (matcher.find()) {
                     ramSize = Integer.parseInt(matcher.group(1));
+
+                    ramName = ramName.replaceAll("\\d+\\s*(GB)", "").trim();
+
+                    //System.out.println(ramId + " || " + ramName + " || " + ramLatency + " || " + ramRead + " || " + ramWrite);
+
+                    if (ramLatency <= 70 && !ramName.equals("OM Nanotech Pvt.Ltd V1D4L2G82G83200") && !ramName.equals("OM Nanotech Pvt.Ltd V1D4S2G82G83200")) {
+                        RamList ramList = RamList.builder()
+                                .ramId(ramId)
+                                .ramName(ramName)
+                                .ramType(ramType)
+                                .ramSize(ramSize)
+                                .ramLatency(ramLatency)
+                                .ramRead(ramRead)
+                                .ramWrite(ramWrite)
+                                .build();
+                        insertRamList.save(ramList);
+                    }
                 }
-                ramName = ramName.replaceAll("\\d+\\s*(GB|MB|TB|KB)", "").trim();
+            }
+        }
+        return "success!!";
+    }
 
-                //System.out.println(ramId + " || " + ramName + " || " + ramLatency + " || " + ramRead + " || " + ramWrite);
+    @GetMapping("/ram_list2")
+    public String getRamList2() throws IOException {
+        String url = "https://www.memorybenchmark.net/ram_list.php";
+        Document document = Jsoup.connect(url).get();
+        Element tbody = document.select("tbody").first();
+        Elements rows = tbody.select("tr");
 
-                if(ramLatency<=70){
-                    RamList ramList = RamList.builder()
-                            .ramId(ramId)
-                            .ramName(ramName)
-                            .ramType(ramType)
-                            .ramSize(ramSize)
-                            .ramLatency(ramLatency)
-                            .ramRead(ramRead)
-                            .ramWrite(ramWrite)
-                            .build();
-                    insertRamList.save(ramList);
+        // id 범위 지정
+        int start = 1;
+        int end = 23000;
+        String ramType = "ddr5";
+
+        for (Element row : rows) {
+            Element link = row.select("td:first-child a").first();
+            String href = link.attr("href");
+            int ramId = Integer.parseInt(href.split("&id=")[1]);
+
+            if (ramId >= start && ramId <= end) {
+                String ramName = link.text();
+                int ramLatency = Integer.parseInt(row.select("td:nth-child(2)").text());
+                double ramRead = Double.parseDouble(row.select("td:nth-child(3)").text());
+                double ramWrite = Double.parseDouble(row.select("td:nth-child(4)").text());
+
+                int ramSize = 0;
+                Pattern pattern = Pattern.compile("(\\d+)\\s*(GB)");
+                Matcher matcher = pattern.matcher(ramName);
+                if (matcher.find()) {
+                    ramSize = Integer.parseInt(matcher.group(1));
+
+                    ramName = ramName.replaceAll("\\d+\\s*(GB)", "").trim();
+
+                    //System.out.println(ramId + " || " + ramName + " || " + ramLatency + " || " + ramRead + " || " + ramWrite);
+
+                    if (ramLatency <= 70) {
+                        RamList ramList = RamList.builder()
+                                .ramId(ramId)
+                                .ramName(ramName)
+                                .ramType(ramType)
+                                .ramSize(ramSize)
+                                .ramLatency(ramLatency)
+                                .ramRead(ramRead)
+                                .ramWrite(ramWrite)
+                                .build();
+                        insertRamList.save(ramList);
+                    }
                 }
             }
         }
@@ -174,6 +227,31 @@ public class CrawlingController {
             }
         }
         return "success!!";
+    }
+
+    @GetMapping("/gpu_list2")
+    public void updateGpuImageUrls() throws IOException {
+        List<GpuList> gpuList = insertGpuList.findAll(); // DB에서 저장한 GPU 목록 가져오기
+
+        for (GpuList gpu : gpuList) {
+            String query = gpu.getGpuName() + " gpu"; // 검색할 쿼리
+            String imageUrl = getImageUrlFromGoogle(query); // 구글에서 이미지 URL 가져오기
+            gpu.setGpuUrl(imageUrl); // GPU 객체에 이미지 URL 저장
+            insertGpuList.save(gpu); // DB에 저장
+        }
+    }
+
+    private String getImageUrlFromGoogle(String query) throws IOException {
+        String url = "https://www.google.com/search?q=" + query + "&tbm=isch";
+        Document document = Jsoup.connect(url).get();
+        Elements images = document.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
+        if (images.size() > 0) {
+            Element image = images.get(0);
+            String imageUrl = image.absUrl("src");
+            return imageUrl;
+        } else {
+            return null;
+        }
     }
 }
 
