34857dc0b2f1d1f5204de6e47e87027eb2cd51eb
seaking <seaking110@naver.com>
2023-04-02 21:52:05 +0900

    [Mod] ShowMySpec.js 다운로드 버튼 [add] MySpec.js

--- src/main/frontend/src/components/eventBanner/ShowMySpec.js
+++ src/main/frontend/src/components/eventBanner/ShowMySpec.js
@@ -1,46 +1,53 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
+import styles from "./eventBanner.module.css";
 
 function ShowMySpec() {
-    const [userSpec, setUserSpec] = useState([]);
     let waring = "시스템 정보를 불러오기 위한 파일을 다운로드 중입니다. 다운이 완료되면 실행 시켜 주세요"
-    const [warning, setWarning] = useState("")
+
 
     const [data, setData] = useState([]);
+    const downloadFile = async () => {
+        const response = await axios({
+            url: 'http://localhost:12000/ShowMySpec',
+            method: 'GET',
+            responseType: 'blob', // 파일 다운로드를 위한 설정
+        });
+
+        // 파일 다운로드를 위한 코드
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'Scoop.exe');
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode.removeChild(link);
+    };
 
     useEffect(() => {
         const eventSource = new EventSource('/stream-data');
         eventSource.onmessage = event => {
-            console.log(event.data, " data");
-            //const newData = JSON.parse(event.data);
-            //console.log("Recived data:", newData);
             setData(prevData => [...prevData, event.data]);
         };
     }, []);
 
-    useEffect(() => {
-        const userData = async () => {
-            try {
-                const response = await axios.get('/MySpec');
-                setUserSpec(response.data);
-                waring = "시스템 정보 불러오기 완료";
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        userData();
-    },[waring])
+
     return (
         <div>
             <p>
                 {waring}
-                {userSpec}
-                {data.map((item, index) => (
-                    <div key={index}>{item}</div>
-                ))}
+
+                <button className={styles.button} onClick={downloadFile}>download</button>
+
             </p>
+            {data.map((item, index) => (
+                <div key={index}>
+                    <Link to="/MySpec">Go to MySpec</Link>
+                </div>
+                ))}
         </div>
+
     );
 }
 export default ShowMySpec;
