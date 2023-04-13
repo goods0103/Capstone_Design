4c9c2171ad1eab8b313e0836e3b2a210624673c4
seaking <seaking110@naver.com>
2023-04-13 19:42:06 +0900

    [MOD]  UserInfoController 수정 및 InsertSpec 수정

--- src/main/frontend/src/components/detail/CpuCompare.js
+++ src/main/frontend/src/components/detail/CpuCompare.js
@@ -3,8 +3,24 @@ import axios from "axios";
 import styles from "./detail.module.css";
 import Select from "react-select";
 import { useLocation } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import ProgressBarLabel from 'react-bootstrap/ProgressBar';
 
-
+const data1 = [
+    { name: 'Benchmark', value: 21945 },
+    { name: 'ClockSpeed', value: 3.7 },
+    { name: 'TurboSpeed', value: 4.6 },
+    { name: 'Cores', value: 6 },
+    { name: 'Price', value: 187956 },
+];
+const data2 = [
+    { name: 'Benchmark', value: 11945 },
+    { name: 'ClockSpeed', value: 2.7 },
+    { name: 'TurboSpeed', value: 3.6 },
+    { name: 'Cores', value: 4 },
+    { name: 'Price', value: 137956 },
+];
 function CpuCompare() {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
@@ -56,6 +72,7 @@ function CpuCompare() {
 
     return(
         <>
+
             <div className={styles.container1}>
                 <div className={styles.item}>선택된 스펙이름</div>
                 <form onSubmit={handleSubmit} className={styles.item} >
@@ -75,9 +92,46 @@ function CpuCompare() {
 
             </div>
             <div className={styles.container2}>
-                <div className={styles.item}>선택된 스펙 정보 출력</div>
-                <div className={styles.item}>선택할 스펙 정보 출력</div>
+                <div className={styles.item}>선택된 스펙 정보 출력
+
+                </div>
+                <div className={styles.item}>선택할 스펙 정보 출력
+
+
+
+                </div>
             </div>
+            <label>benchmark</label>
+            <ProgressBar style={{ height: '30px' }}>
+                <label>{data1.find(obj => obj.name === 'Benchmark').value}</label>
+                <ProgressBar variant="success" now={data1.find(obj => obj.name === 'Benchmark').value/(data1.find(obj => obj.name === 'Benchmark').value+data2.find(obj => obj.name === 'Benchmark').value)*100} key={1} label={data1.find(obj => obj.name === 'Benchmark').value/(data1.find(obj => obj.name === 'Benchmark').value+data2.find(obj => obj.name === 'Benchmark').value)}/>
+                <ProgressBar  variant="danger" now={data2.find(obj => obj.name === 'Benchmark').value/(data1.find(obj => obj.name === 'Benchmark').value+data2.find(obj => obj.name === 'Benchmark').value)*100} key={2} label={data2.find(obj => obj.name === 'Benchmark').value/(data1.find(obj => obj.name === 'Benchmark').value+data2.find(obj => obj.name === 'Benchmark').value)}/>
+                <label>{data2.find(obj => obj.name === 'Benchmark').value}</label>
+            </ProgressBar>
+            <br/>
+            <ProgressBar>
+                <label>ClockSpeed</label>
+                <ProgressBar  variant="success" now={45} key={1} />
+
+                <ProgressBar  variant="danger" now={55} key={2} />
+            </ProgressBar>
+            <br/>
+            <ProgressBar>
+                <label>TurboSpeed</label>
+                <ProgressBar  variant="success" now={75} key={1} />
+                <ProgressBar  variant="danger" now={25} key={2} />
+            </ProgressBar>
+            <br/>
+            <ProgressBar>
+                <label>Cores</label>
+                <ProgressBar striped variant="success" now={55} key={1} />
+                <ProgressBar striped variant="danger" now={45} key={2} />
+            </ProgressBar>
+            <ProgressBar>
+                <label>Price</label>
+                <ProgressBar striped variant="success" now={55} key={1} />
+                <ProgressBar striped variant="danger" now={45} key={2} />
+            </ProgressBar>
         </>
     );
 }
