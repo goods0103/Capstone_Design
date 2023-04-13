8054d69adb3a1329329e4f98c49e874958aac44f
suho <yousuho1232@naver.com>
2023-04-13 19:39:30 +0900

    [Mod] CpuCompare.js(프로그래스바 추가)

--- src/main/frontend/src/components/detail/CpuCompare.js
+++ src/main/frontend/src/components/detail/CpuCompare.js
@@ -3,6 +3,11 @@ import axios from "axios";
 import styles from "./detail.module.css";
 import Select from "react-select";
 import { useLocation } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 
 function CpuCompare() {
@@ -29,11 +34,6 @@ function CpuCompare() {
             });
     }, []);
 
-
-
-
-
-
     const handleSubmit = (e) => {
         e.preventDefault();
     };
@@ -52,13 +52,38 @@ function CpuCompare() {
             });
     }, []);
 
+    const CpuInfoProgressBar = (info1, info2) => {
+        let a, b;
+        a = info1/(info1 + info2) * 100;
+        b = info2/(info1 + info2) * 100;
+        a = Math.round(a);
+        b = Math.round(b);
+
+        return (
+            <ProgressBar className={styles.progressBarCss} style={{height:'30px'}}>
+                <ProgressBar animated variant="success" now={a} label={`${a}%(${info1})`} key={1} />
+                <ProgressBar animated variant="warning" now={b} label={`${b}%(${info2})`} key={2} />
+            </ProgressBar>
+        );
+    }
 
 
     return(
         <>
+            {/*<Container>*/}
+            {/*    <Row>*/}
+            {/*        <Col>1 of 2</Col>*/}
+            {/*        <Col>2 of 2</Col>*/}
+            {/*    </Row>*/}
+            {/*    <Row>*/}
+            {/*        <Col>1 of 3</Col>*/}
+            {/*        <Col>2 of 3</Col>*/}
+            {/*        <Col>3 of 3</Col>*/}
+            {/*    </Row>*/}
+            {/*</Container>*/}
             <div className={styles.container1}>
-                <div className={styles.item}>선택된 스펙이름</div>
-                <form onSubmit={handleSubmit} className={styles.item} >
+                <div className={styles.itemLabel}>선택된 스펙이름</div>
+                <form onSubmit={handleSubmit} className={styles.itemForm} >
                     <label>원하는 Cpu를 입력하세요 : </label>
                     <Select
                         value={selectedCpu}
@@ -75,9 +100,52 @@ function CpuCompare() {
 
             </div>
             <div className={styles.container2}>
-                <div className={styles.item}>선택된 스펙 정보 출력</div>
-                <div className={styles.item}>선택할 스펙 정보 출력</div>
+                <div className={styles.itemLabel}>선택된 스펙 정보 출력</div>
+                <div className={styles.itemLabel}>선택할 스펙 정보 출력</div>
             </div>
+
+            <label>Cpu BenchMark &nbsp;</label><br/>
+            {/*<ProgressBar className={styles.progressBarCss}>*/}
+            {/*    <ProgressBar animated variant="success" now={35} label={`${35}%`} key={1} />*/}
+            {/*    <ProgressBar animated variant="warning" now={65} label={`${65}%`} key={2} />*/}
+            {/*</ProgressBar>*/}
+            {CpuInfoProgressBar(2335, 7093)}
+            <br/>
+            <label>Cpu Clock Speed &nbsp;</label><br/>
+            {/*<ProgressBar className={styles.progressBarCss}>*/}
+            {/*    <ProgressBar animated variant="success" now={45} label={`${45}%`} key={1} />*/}
+            {/*    <ProgressBar animated variant="warning" now={55} label={`${55}%`} key={2} />*/}
+            {/*</ProgressBar>*/}
+            {CpuInfoProgressBar(1245, 4523)}
+            <br/>
+            <label>Cpu Turbo Speed &nbsp;</label><br/>
+            {/*<ProgressBar className={styles.progressBarCss}>*/}
+            {/*    <ProgressBar animated variant="success" now={75} label={`${75}%`} key={1} />*/}
+            {/*    <ProgressBar animated variant="warning" now={25} label={`${25}%`} key={2} />*/}
+            {/*</ProgressBar>*/}
+            {CpuInfoProgressBar(34, 12)}
+            <br/>
+            <label>Cpu Cores &nbsp;</label><br/>
+            {/*<ProgressBar className={styles.progressBarCss}>*/}
+            {/*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*/}
+            {/*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*/}
+            {/*</ProgressBar>*/}
+            {CpuInfoProgressBar(673, 234)}
+            <br/>
+            <label>Cpu Price &nbsp;</label><br/>
+            {/*<ProgressBar className={styles.progressBarCss}>*/}
+            {/*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*/}
+            {/*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*/}
+            {/*</ProgressBar>*/}
+            {CpuInfoProgressBar(2542, 8655)}
+            <br/>
+            <label>Cpu Value &nbsp;</label><br/>
+            {/*<ProgressBar className={styles.progressBarCss}>*/}
+            {/*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*/}
+            {/*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*/}
+            {/*</ProgressBar>*/}
+            {CpuInfoProgressBar(1212, 3232)}
+            <br/>
         </>
     );
 }
--- src/main/frontend/src/components/detail/CpuDetail.js
+++ src/main/frontend/src/components/detail/CpuDetail.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./detail.module.css";
 import {Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function CpuDetail() {
     const [cpuValue, setCpuValue] = useState([]);
