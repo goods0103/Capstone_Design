09a7858a1973b638079139e3f5061df37df56cc3
suho <yousuho1232@naver.com>
2023-04-30 15:55:39 +0900

    [Mod] cpuCompare.js, SubmitSelectedCpu.js

--- src/main/frontend/src/components/detail/CpuCompare.js
+++ src/main/frontend/src/components/detail/CpuCompare.js
@@ -27,6 +27,12 @@ function CpuCompare() {
     const [selectedCpuInfoDetail, setSelectedCpuInfoDetail] = useState([]);
 
     const [showComponent, setShowComponent] = useState(false);
+    const [renderCount, setRenderCount] = useState(0);
+
+    useEffect(() => {
+        console.log('Component is rendered');
+        setSelectedCpu(selectedCpu);
+    }, [renderCount]);
 
     useEffect(() => {
         axios.get('/category/cpu_name')
@@ -36,6 +42,7 @@ function CpuCompare() {
                     label: cpus
                 }));
                 setCpuOption(cpus);
+                // setSelectedCpu("");
             })
             .catch(error => {
                 console.log(error);
@@ -48,6 +55,8 @@ function CpuCompare() {
 
     function handleCpuChange(selectedCpu) {
         setSelectedCpu(selectedCpu);
+        // setRenderCount(renderCount + 1);
+        // setShowComponent(false);
     }
 
     useEffect(() => {
@@ -90,22 +99,23 @@ function CpuCompare() {
     //         });
     // }, []);
 
-    const CpuInfoProgressBar = (info1, info2) => {
-        let a, b;
-        a = info1/(info1 + info2) * 100;
-        b = info2/(info1 + info2) * 100;
-        a = Math.round(a);
-        b = Math.round(b);
-
-        return (
-            <ProgressBar className={styles.progressBarCss}>
-                <ProgressBar animated variant="success" now={a} label={`${a}%(${info1})`} key={1} />
-                <ProgressBar animated variant="warning" now={b} label={`${b}%(${info2})`} key={2} />
-            </ProgressBar>
-        );
-    }
+    // const CpuInfoProgressBar = (info1, info2) => {
+    //     let a, b;
+    //     a = info1/(info1 + info2) * 100;
+    //     b = info2/(info1 + info2) * 100;
+    //     a = Math.round(a);
+    //     b = Math.round(b);
+    //
+    //     return (
+    //         <ProgressBar className={styles.progressBarCss}>
+    //             <ProgressBar animated variant="success" now={a} label={`${a}%(${info1})`} key={1} />
+    //             <ProgressBar animated variant="warning" now={b} label={`${b}%(${info2})`} key={2} />
+    //         </ProgressBar>
+    //     );
+    // }
 
     function handleClick() {
+        setRenderCount(renderCount + 1);
         setSelectedCpu(selectedCpu);
         // if (selectedCpu) {
         //     try {
@@ -202,10 +212,10 @@ function CpuCompare() {
                     <input name = "cpuSelect" className={styles.selectTagShow} value={selectedCpu ? selectedCpu.label : ''} />
                     <br/>
                     <button type="submit" onClick={handleClick} className={styles.buttonSubmit}>비교하기</button>
-                    <SubmitSelectedCpu selectedCpu={selectedCpu}
-                                       setSelectedCpuInfo={setSelectedCpuInfo}
-                                       setSelectedCpuInfoDetail={setSelectedCpuInfoDetail}
-                                       setShowComponent={setShowComponent}/>
+                    {/*<SubmitSelectedCpu selectedCpu={selectedCpu}*/}
+                    {/*                   setSelectedCpuInfo={setSelectedCpuInfo}*/}
+                    {/*                   setSelectedCpuInfoDetail={setSelectedCpuInfoDetail}*/}
+                    {/*                   setShowComponent={setShowComponent}/>*/}
                 </form>
             </div>
             <div className={styles.container2}>
@@ -213,51 +223,61 @@ function CpuCompare() {
                 <div className={styles.itemLabel}>선택할 스펙 정보 출력</div>
             </div>
 
+            {/*{showComponent &&*/}
+            {/*<div>*/}
+            {/*    <label>Cpu BenchMark &nbsp;</label><br/>*/}
+            {/*    /!*<ProgressBar className={styles.progressBarCss}>*!/*/}
+            {/*    /!*    <ProgressBar animated variant="success" now={35} label={`${35}%`} key={1} />*!/*/}
+            {/*    /!*    <ProgressBar animated variant="warning" now={65} label={`${65}%`} key={2} />*!/*/}
+            {/*    /!*</ProgressBar>*!/*/}
+            {/*    {CpuInfoProgressBar(cpuInfo.cpuMark, selectedCpuInfoDetail.cpuMark)}*/}
+            {/*    <br/>*/}
+            {/*    <label>Cpu Clock Speed &nbsp;</label><br/>*/}
+            {/*    /!*<ProgressBar className={styles.progressBarCss}>*!/*/}
+            {/*    /!*    <ProgressBar animated variant="success" now={45} label={`${45}%`} key={1} />*!/*/}
+            {/*    /!*    <ProgressBar animated variant="warning" now={55} label={`${55}%`} key={2} />*!/*/}
+            {/*    /!*</ProgressBar>*!/*/}
+            {/*    {CpuInfoProgressBar(cpuInfo2.clock, selectedCpuInfoDetail.clock)}*/}
+            {/*    <br/>*/}
+            {/*    <label>Cpu Turbo Speed &nbsp;</label><br/>*/}
+            {/*    /!*<ProgressBar className={styles.progressBarCss}>*!/*/}
+            {/*    /!*    <ProgressBar animated variant="success" now={75} label={`${75}%`} key={1} />*!/*/}
+            {/*    /!*    <ProgressBar animated variant="warning" now={25} label={`${25}%`} key={2} />*!/*/}
+            {/*    /!*</ProgressBar>*!/*/}
+            {/*    {CpuInfoProgressBar(cpuInfo2.turbo, selectedCpuInfoDetail.turbo)}*/}
+            {/*    <br/>*/}
+            {/*    <label>Cpu Cores &nbsp;</label><br/>*/}
+            {/*    /!*<ProgressBar className={styles.progressBarCss}>*!/*/}
+            {/*    /!*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*!/*/}
+            {/*    /!*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*!/*/}
+            {/*    /!*</ProgressBar>*!/*/}
+            {/*    {CpuInfoProgressBar(cpuInfo2.core, selectedCpuInfoDetail.core)}*/}
+            {/*    <br/>*/}
+            {/*    <label>Cpu Price &nbsp;</label><br/>*/}
+            {/*    /!*<ProgressBar className={styles.progressBarCss}>*!/*/}
+            {/*    /!*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*!/*/}
+            {/*    /!*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*!/*/}
+            {/*    /!*</ProgressBar>*!/*/}
+            {/*    {CpuInfoProgressBar(cpuInfo.cpuPrice, selectedCpuInfo.cpuPrice)}*/}
+            {/*    <br/>*/}
+            {/*    <label>Cpu Value &nbsp;</label><br/>*/}
+            {/*    /!*<ProgressBar className={styles.progressBarCss}>*!/*/}
+            {/*    /!*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*!/*/}
+            {/*    /!*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*!/*/}
+            {/*    /!*</ProgressBar>*!/*/}
+            {/*    {CpuInfoProgressBar(cpuInfo.cpuValue, selectedCpuInfo.cpuValue)}*/}
+            {/*    <br/>*/}
+            {/*</div>*/}
+            {/*}*/}
+
+            {/*<SubmitSelectedCpu selectedCpu={selectedCpu}*/}
+            {/*                   cpuInfo={cpuInfo}*/}
+            {/*                   cpuInfo2={cpuInfo2}/>*/}
+
             {showComponent &&
-            <div>
-                <label>Cpu BenchMark &nbsp;</label><br/>
-                {/*<ProgressBar className={styles.progressBarCss}>*/}
-                {/*    <ProgressBar animated variant="success" now={35} label={`${35}%`} key={1} />*/}
-                {/*    <ProgressBar animated variant="warning" now={65} label={`${65}%`} key={2} />*/}
-                {/*</ProgressBar>*/}
-                {CpuInfoProgressBar(cpuInfo.cpuMark, selectedCpuInfoDetail.cpuMark)}
-                <br/>
-                <label>Cpu Clock Speed &nbsp;</label><br/>
-                {/*<ProgressBar className={styles.progressBarCss}>*/}
-                {/*    <ProgressBar animated variant="success" now={45} label={`${45}%`} key={1} />*/}
-                {/*    <ProgressBar animated variant="warning" now={55} label={`${55}%`} key={2} />*/}
-                {/*</ProgressBar>*/}
-                {CpuInfoProgressBar(cpuInfo2.clock, selectedCpuInfoDetail.clock)}
-                <br/>
-                <label>Cpu Turbo Speed &nbsp;</label><br/>
-                {/*<ProgressBar className={styles.progressBarCss}>*/}
-                {/*    <ProgressBar animated variant="success" now={75} label={`${75}%`} key={1} />*/}
-                {/*    <ProgressBar animated variant="warning" now={25} label={`${25}%`} key={2} />*/}
-                {/*</ProgressBar>*/}
-                {CpuInfoProgressBar(cpuInfo2.turbo, selectedCpuInfoDetail.turbo)}
-                <br/>
-                <label>Cpu Cores &nbsp;</label><br/>
-                {/*<ProgressBar className={styles.progressBarCss}>*/}
-                {/*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*/}
-                {/*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*/}
-                {/*</ProgressBar>*/}
-                {CpuInfoProgressBar(cpuInfo2.core, selectedCpuInfoDetail.core)}
-                <br/>
-                <label>Cpu Price &nbsp;</label><br/>
-                {/*<ProgressBar className={styles.progressBarCss}>*/}
-                {/*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*/}
-                {/*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*/}
-                {/*</ProgressBar>*/}
-                {CpuInfoProgressBar(cpuInfo.cpuPrice, selectedCpuInfo.cpuPrice)}
-                <br/>
-                <label>Cpu Value &nbsp;</label><br/>
-                {/*<ProgressBar className={styles.progressBarCss}>*/}
-                {/*    <ProgressBar animated variant="success" now={55} label={`${55}%`} key={1} />*/}
-                {/*    <ProgressBar animated variant="warning" now={45} label={`${45}%`} key={2} />*/}
-                {/*</ProgressBar>*/}
-                {CpuInfoProgressBar(cpuInfo.cpuValue, selectedCpuInfo.cpuValue)}
-                <br/>
-            </div>
+                <SubmitSelectedCpu selectedCpu={selectedCpu}
+                                   cpuInfo={cpuInfo}
+                                   cpuInfo2={cpuInfo2}/>
             }
 
         </>
--- src/main/frontend/src/components/detail/SubmitSelectedCpu.js
+++ src/main/frontend/src/components/detail/SubmitSelectedCpu.js
@@ -1,18 +1,69 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import styles from "./detail.module.css";
 
 
-function SubmitSelectedCpu({ selectedCpu, setSelectedCpuInfo, setSelectedCpuInfoDetail, setShowComponent }) {
+function SubmitSelectedCpu({ selectedCpu, cpuInfo, cpuInfo2 }) {
 
-    // const [selectedCpuInfo, setSelectedCpuInfo] = useState([]);
-    // const [selectedCpuInfoDetail, setSelectedCpuInfoDetail] = useState([]);
+    const [selectedCpuInfo, setSelectedCpuInfo] = useState([]);
+    const [selectedCpuInfoDetail, setSelectedCpuInfoDetail] = useState([]);
+
+    // selectedCpuInfo
+    const [selectedCpuInfoPrice, setSelectedCpuInfoPrice] = useState(0);
+    const [selectedCpuInfoValue, setSelectedCpuInfoValue] = useState(0);
+    const [selectedCpuInfoMark, setSelectedCpuInfoMark] = useState(0);
+
+    // cpuInfo
+    const [cpuInfoPrice, setCpuInfoPrice] = useState(0);
+    const [cpuInfoValue, setCpuInfoValue] = useState(0);
+    const [cpuInfoMark, setCpuInfoMark] = useState(0);
+
+    // selectedCpuInfoDetail
+    const [selectedCpuInfoDetailClock, setSelectedCpuInfoDetailClock] = useState(0);
+    const [selectedCpuInfoDetailTurbo, setSelectedCpuInfoDetailTurbo] = useState(0);
+    const [selectedCpuInfoDetailCore, setSelectedCpuInfoDetailCore] = useState(0);
+
+    // cpuInfoDetail
+    const [cpuInfoDetailClock, setCpuInfoDetailClock] = useState(0);
+    const [cpuInfoDetailTurbo, setCpuInfoDetailTurbo] = useState(0);
+    const [cpuInfoDetailCore, setCpuInfoDetailCore] = useState(0);
+
+    const CpuInfoProgressBar = (info1, info2) => {
+        let a, b;
+        a = info1/(info1 + info2) * 100;
+        b = info2/(info1 + info2) * 100;
+        a = Math.round(a);
+        b = Math.round(b);
+
+        return (
+            <div>
+                {a > 0 && b > 0 && !isNaN(a) && !isNaN(b) &&
+                    <div>
+                        <ProgressBar className={styles.progressBarCss}>
+                            <ProgressBar animated variant="success" now={a} label={`${a}%(${info1})`} key={1} />
+                            <ProgressBar animated variant="warning" now={b} label={`${b}%(${info2})`} key={2} />
+                        </ProgressBar>
+                    </div>
+                }
+            </div>
+            // <div>
+            //     <ProgressBar className={styles.progressBarCss}>
+            //         <ProgressBar animated variant="success" now={a} label={`${a}%(${info1})`} key={1} />
+            //         <ProgressBar animated variant="warning" now={b} label={`${b}%(${info2})`} key={2} />
+            //     </ProgressBar>
+            // </div>
+        );
+    }
 
     useEffect(() => {
         if (selectedCpu) {
             const fetchData = async () => {
                 try {
-                    const response = await axios.post('/find_cpu_detail_name', `${selectedCpu}`);
+                    const response = await axios.post('/find_cpu_name', `${selectedCpu.label}`);
                     setSelectedCpuInfo(response.data);
+                    changeSelectedCpuInfoToInt(selectedCpuInfo);
+                    changeCpuInfoToInt(cpuInfo);
                 } catch (error) {
                     console.log(error);
                 }
@@ -25,8 +76,10 @@ function SubmitSelectedCpu({ selectedCpu, setSelectedCpuInfo, setSelectedCpuInfo
         if (selectedCpu) {
             const fetchData = async () => {
                 try {
-                    const response = await axios.post('/find_cpu_detail_name', `${selectedCpu}`);
+                    const response = await axios.post('/find_cpu_detail_name', `${selectedCpu.label}`);
                     setSelectedCpuInfoDetail(response.data);
+                    changeSelectedCpuInfoDetailToInt(selectedCpuInfoDetail);
+                    changeCpuInfoDetailToInt(cpuInfo2);
                 } catch (error) {
                     console.log(error);
                 }
@@ -35,8 +88,132 @@ function SubmitSelectedCpu({ selectedCpu, setSelectedCpuInfo, setSelectedCpuInfo
         }
     }, [selectedCpu]);
 
-    setShowComponent(true);
-    return null;
+    useEffect(() => {
+        console.log(selectedCpu);
+    }, []);
+
+    const changeSelectedCpuInfoToInt = (cpuInfo) => {
+        // cpuInfo.cpuPrice, cpuInfo.cpuValue
+        const strPrice = cpuInfo.cpuPrice;
+        const strValue = cpuInfo.cpuValue;
+        const strMark = cpuInfo.cpuMark;
+
+        const intPrice = parseInt(strPrice);
+        const intValue = parseInt(strValue);
+        const intMark = parseInt(strMark);
+
+        setSelectedCpuInfoPrice(intPrice);
+        setSelectedCpuInfoValue(intValue);
+        setSelectedCpuInfoMark(intMark);
+    }
+
+    const changeCpuInfoToInt = (cpuInfo) => {
+        // cpuInfo.cpuPrice, cpuInfo.cpuValue
+        const strPrice = cpuInfo.cpuPrice;
+        const strValue = cpuInfo.cpuValue;
+        const strMark = cpuInfo.cpuMark;
+
+        const intPrice = parseInt(strPrice);
+        const intValue = parseInt(strValue);
+        const intMark = parseInt(strMark);
+
+        setCpuInfoPrice(intPrice);
+        setCpuInfoValue(intValue);
+        setCpuInfoMark(intMark);
+    }
+
+    const changeSelectedCpuInfoDetailToInt = (cpuInfoDetail) => {
+        // cpuInfoDetail.clock, cpuInfoDetail.turbo, cpuInfoDetail.core
+        let strClock2 = cpuInfoDetail.clock;
+        let strTurbo2 = cpuInfoDetail.turbo;
+        const strCore = cpuInfoDetail.core;
+
+        const strClock = strClock2.split(' ')[0];
+        const strTurbo = strTurbo2.split(' ')[0];
+
+        const intClock = parseFloat(strClock);
+        const intTurbo = parseFloat(strTurbo);
+        const intCore = parseInt(strCore);
+
+        setCpuInfoDetailClock(intClock);
+        setCpuInfoDetailTurbo(intTurbo);
+        setCpuInfoDetailCore(intCore);
+    }
+
+    const changeCpuInfoDetailToInt = (cpuInfoDetail) => {
+        // cpuInfoDetail.clock, cpuInfoDetail.turbo, cpuInfoDetail.core
+        let strClock2 = cpuInfoDetail.clock;
+        let strTurbo2 = cpuInfoDetail.turbo;
+        const strCore = cpuInfoDetail.core;
+
+        const strClock = strClock2.split(' ')[0];
+        const strTurbo = strTurbo2.split(' ')[0];
+
+        const intClock = parseFloat(strClock);
+        const intTurbo = parseFloat(strTurbo);
+        const intCore = parseInt(strCore);
+
+        setSelectedCpuInfoDetailClock(intClock);
+        setSelectedCpuInfoDetailTurbo(intTurbo);
+        setSelectedCpuInfoDetailCore(intCore);
+    }
+
+
+
+    return (
+        <>
+            <div>
+                {(cpuInfoMark > 0 && selectedCpuInfoMark > 0 && !isNaN(cpuInfoMark) && !isNaN(selectedCpuInfoMark)) &&
+                    <div>
+                        <label>Cpu BenchMark &nbsp;</label><br/>
+                        {CpuInfoProgressBar(cpuInfoMark, selectedCpuInfoMark)}
+                        <br/>
+                    </div>
+                }
+
+                {(cpuInfoDetailClock > 0 && selectedCpuInfoDetailClock > 0 && !isNaN(cpuInfoDetailClock) && !isNaN(selectedCpuInfoDetailClock)) &&
+                    <div>
+                        <label>Cpu Clock Speed &nbsp;</label><br/>
+                        {CpuInfoProgressBar(cpuInfoDetailClock, selectedCpuInfoDetailClock)}
+                        <br/>
+                    </div>
+                }
+
+                {(cpuInfoDetailTurbo > 0 && selectedCpuInfoDetailTurbo > 0 && !isNaN(cpuInfoDetailTurbo) && !isNaN(selectedCpuInfoDetailTurbo)) &&
+                    <div>
+                        <label>Cpu Turbo Speed &nbsp;</label><br/>
+                        {CpuInfoProgressBar(cpuInfoDetailTurbo, selectedCpuInfoDetailTurbo)}
+                        <br/>
+                    </div>
+                }
+
+                {(cpuInfoDetailCore > 0 && selectedCpuInfoDetailCore > 0 && !isNaN(cpuInfoDetailCore) && !isNaN(selectedCpuInfoDetailCore)) &&
+                    <div>
+                        <label>Cpu Cores &nbsp;</label><br/>
+                        {CpuInfoProgressBar(cpuInfoDetailCore, selectedCpuInfoDetailCore)}
+                        <br/>
+                    </div>
+                }
+
+                {(cpuInfoPrice > 0 && selectedCpuInfoPrice > 0 && !isNaN(cpuInfoPrice) && !isNaN(selectedCpuInfoPrice)) &&
+                    <div>
+                        <label>Cpu Price &nbsp;</label><br/>
+                        {CpuInfoProgressBar(cpuInfoPrice, selectedCpuInfoPrice)}
+                        <br/>
+                    </div>
+                }
+
+                {(cpuInfoValue > 0 && selectedCpuInfoValue > 0 && !isNaN(cpuInfoValue) && !isNaN(selectedCpuInfoValue)) &&
+                    <div>
+                        <label>Cpu Value &nbsp;</label><br/>
+                        {CpuInfoProgressBar(cpuInfoValue, selectedCpuInfoValue)}
+                        <br/>
+                    </div>
+                }
+
+            </div>
+        </>
+    );
 }
 
 export default SubmitSelectedCpu;
