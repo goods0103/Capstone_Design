d95a09ba0af01609adaebd9d34b4c440e867d619
goods0103 <goods0103@naver.com>
2023-04-30 20:42:12 +0900

    게임별 최소, 권장사양 기준 적정여부 return

--- src/main/java/com/hadoop/demo/Controller/GameController.java
+++ src/main/java/com/hadoop/demo/Controller/GameController.java
@@ -1,10 +1,14 @@
 package com.hadoop.demo.Controller;
 
 import com.hadoop.demo.Service.GameListService;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -13,10 +17,41 @@ public class GameController {
     @Autowired
     private GameListService gameListService;
 
+//    @Getter
+//    @Setter
+//    @AllArgsConstructor
+//    @NoArgsConstructor
+//    @ToString
+//    public static class GameSpec {
+//        private String gameName;
+//        private int minState;
+//        private int recState;
+//    }
+
     @GetMapping("/compare")
     public java.util.List<GameListService.gameUserCompare> GameCpuCompare(){
         gameListService.CompareCpuUserVsGame().stream().forEach(System.out::println);
 
         return gameListService.CompareCpuUserVsGame();
     }
+
+    @GetMapping("/compare2")
+    public java.util.List<GameListService.gameUserCompare> GameCpuCompare2(){
+        gameListService.CompareCpuUserVsGame2().stream().forEach(System.out::println);
+
+        return gameListService.CompareCpuUserVsGame2();
+
+    }
+
+//    @GetMapping("/compare3")
+//    public List<GameSpec> GameCpuCompare3(){
+//        List<GameSpec> gameSpecs = new ArrayList<>();
+//
+//        for(int i=0; i<gameListService.CompareCpuUserVsGame2().size();i++){
+//            gameSpecs.add(new GameSpec(gameListService.CompareCpuUserVsGame2().get(i).getGameName(),
+//                    gameListService.CompareCpuUserVsGame().get(i).getState(), gameListService.CompareCpuUserVsGame2().get(i).getState()));
+//        }
+//
+//        return gameSpecs;
+//    }
 }
