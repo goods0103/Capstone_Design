d3f8b26e655c3b13864372a33642007e8a6e20a0
suho <yousuho1232@naver.com>
2023-05-10 18:14:19 +0900

    [Mod] ShowInsertInfo.jsx, ShowInsertInfoRam.jsx, category.module.css 다자인 변경 [Add] ShowInsertInfoGpu.jsx

---
+++ src/main/frontend/src/components/insertInfo/ShowInsertInfoGpu.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import styles from "./category.module.css"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import {Link} from "react-router-dom";
+import {ListGroup} from "react-bootstrap";
+
+export default function ShowInsertInfoGpu({infoName, infoMark, infoRank, infoValue, infoPrice, infoUrl, infoId}) {
+    return(
+        <div className={styles.divChild}>
+            <div className={styles.infoSpecTitle}>GPU</div>
+            <img
+                className={styles.image}
+                src={infoUrl}
+                alt="gpu_image"
+            /><br/><br/>
+            <div className={styles.infoSpecTitle}>{infoName}</div>
+                <div className={styles.urlLabel}><Link to={`/GpuSpec/${infoId}`} className={styles.linkUrl}>더 알아보기 ></Link></div>
+            <hr className={styles.hrStyle}/>
+            <div className={styles.infoSpecSubTitle}>Bench Mark</div>
+            <div className={styles.infoSpecSub}>{infoMark}</div>
+            <div className={styles.infoSpecSubTitle}>Rank</div>
+            <div className={styles.infoSpecSub}>{infoRank}</div>
+            <div className={styles.infoSpecSubTitle}>Value</div>
+            <div className={styles.infoSpecSub}>{infoValue}</div>
+            <div className={styles.infoSpecSubTitle}>Price</div>
+            <div className={styles.infoSpecSub}>{infoPrice}</div>
+            <hr className={styles.hrStyle}/>
+        </div>
+
+    );
+}
