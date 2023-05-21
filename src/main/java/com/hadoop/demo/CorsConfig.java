f020f92e67b04e8be3be9963bc81aec859471c5b
goods0103 <goods0103@naver.com>
2023-05-21 17:22:38 +0900

    final 2

--- src/main/java/com/hadoop/demo/CorsConfig.java
+++ src/main/java/com/hadoop/demo/CorsConfig.java
@@ -1,8 +1,11 @@
 package com.hadoop.demo;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -12,4 +15,15 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
     }
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> filterRegistrationBean =
+                new FilterRegistrationBean<>(new ForwardedHeaderFilter());
+        filterRegistrationBean.setOrder(0);
+        return filterRegistrationBean;
+    }
+
+
+
 }
