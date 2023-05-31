aafdd0112ddf7ed032d444690904b88d4c0feca6
suho <yousuho1232@naver.com>
2023-05-31 17:46:41 +0900

    [Mod] BottleNeck 부분 소수점 해결

--- src/main/frontend/src/components/insertInfo/InsertInfoBottleNeck.jsx
+++ src/main/frontend/src/components/insertInfo/InsertInfoBottleNeck.jsx
@@ -64,24 +64,24 @@ function InsertInfoBottleNeck(props) {
             <div className={styles.bottleNeckComp}>
                 <div className={styles.bottleNeckText}>
                 <p className={styles.bottleNeckTitle}>CALCULATOR RESULT</p>
-                    {bottleNeck.cpuBottleNeckValue-bottleNeck.gpuBottleNeckValue>5 &&(
+                    {parseFloat(((bottleNeck.cpuBottleNeckValue)/10).toFixed(2))-parseFloat(((bottleNeck.gpuBottleNeckValue)/10).toFixed(2))>5 &&(
                         <p> 작업을 실행할 때 {bottleNeck.cpuInfo}는 {bottleNeck.gpuInfo}에 비해 약합니다.</p>
                         )}
-                    {bottleNeck.gpuBottleNeckValue-bottleNeck.cpuBottleNeckValue>5 &&(
+                    {parseFloat(((bottleNeck.gpuBottleNeckValue)/10).toFixed(2))-parseFloat(((bottleNeck.cpuBottleNeckValue)/10).toFixed(2))>5 &&(
                         <p> 작업을 실행할 때 {bottleNeck.gpuInfo}는 {bottleNeck.cpuInfo}에 비해 약합니다.</p>
                     )}
-                    {Math.abs(bottleNeck.gpuBottleNeckValue - bottleNeck.cpuBottleNeckValue) <= 5 && (
+                    {Math.abs(parseFloat(((bottleNeck.gpuBottleNeckValue)/10).toFixed(2)) - parseFloat(((bottleNeck.cpuBottleNeckValue)/10).toFixed(2))) <= 5 && (
                             <p> 작업을 실행할 때 {bottleNeck.cpuInfo} 및 {bottleNeck.gpuInfo}는 <br/><strong
                                 style={{color: "#116B2A"}}>잘 작동합니다</strong>.</p>
                         )}
-                        <p>이 구성에서는 {Math.abs(bottleNeck.cpuBottleNeckValue-bottleNeck.gpuBottleNeckValue)}%의 병목 현상이 있습니다.</p>
+                        <p>이 구성에서는 {Math.abs(parseFloat(((bottleNeck.cpuBottleNeckValue - bottleNeck.gpuBottleNeckValue)/10).toFixed(2)))}%의 병목 현상이 있습니다.</p>
                     <div className={styles.urlLabel2}><Link to={`/InsertCategoryBottleNeck?gpu=${bottleNeck.gpuInfo}&cpu=${bottleNeck.cpuInfo}`} className={styles.linkUrl}><FontAwesomeIcon icon={faBarsStaggered} bounce /> Learn more</Link></div>
 
                 </div>
                 <div className={styles.CircularChart}>
                             <CircularProgressbar
-                                value={Math.abs(bottleNeck.cpuBottleNeckValue-bottleNeck.gpuBottleNeckValue)}
-                                text={`${Math.abs(bottleNeck.cpuBottleNeckValue-bottleNeck.gpuBottleNeckValue)}%`}
+                                value={Math.abs(parseFloat(((bottleNeck.cpuBottleNeckValue - bottleNeck.gpuBottleNeckValue)/10).toFixed(2)))}
+                                text={`${Math.abs(parseFloat(((bottleNeck.cpuBottleNeckValue - bottleNeck.gpuBottleNeckValue)/10).toFixed(2)))}%`}
                                 circleRatio={0.5}
                                 arcSweepAngle={180}
                                 styles={buildStyles({
